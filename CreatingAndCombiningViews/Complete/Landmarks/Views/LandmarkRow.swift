ba00f6954f816d716af7022ba1e751ea71e8f791
luboolu <kjjy98@naver.com>
2022-05-17 18:08:16 +0900

    BuildingListsAndNavigation

---
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by difine on 2022/05/17.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
