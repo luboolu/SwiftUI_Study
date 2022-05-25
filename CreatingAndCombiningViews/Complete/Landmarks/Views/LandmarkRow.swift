9ea441ccb5c34fe30065cc272632f8adfdf408f0
luboolu <kjjy98@naver.com>
2022-05-25 18:08:52 +0900

    BuildingListsAndNavigation

--- CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkRow.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkRow.swift
@@ -9,22 +9,15 @@
 import SwiftUI
 
 struct LandmarkRow: View {
-    var landmark: Landmark
+    //var landmark: Landmark
 
     var body: some View {
-        HStack {
-            landmark.image
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text(landmark.name)
-
-            Spacer()
-        }
+        Text("Hello, World!")
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow()
     }
 }
